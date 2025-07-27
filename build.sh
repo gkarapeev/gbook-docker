e6222da7e87a0c1f6f041dd4a01e6cc777f11021
ross <ross@localhost.my.domain>
2025-07-27 22:17:40 +0300

    improve build script

--- build.sh
+++ build.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
-pi_base_img="--platform=linux/arm64 alpine:3.22"
-pc_base_img="frolvlad/alpine-glibc:alpine-3.18"
-macos_base_img="frolvlad/alpine-glibc:alpine-3.18"
+pi_base_img="alpine:3.22"
+pc_base_img="frolvlad/alpine-glibc:alpine-3.22"
+macos_base_img="frolvlad/alpine-glibc:alpine-3.22"
 
 case "$1" in
-  pi)   base_img="$pi_base_img" ;;
-  pc)   base_img="$pc_base_img" ;;
-  mac)  base_img="$macos_base_img" ;;
-  *)    echo "Usage: $0 [pi|pc|mac]" && exit 1 ;;
+	pi)
+		base_img="$pi_base_img"
+		platform_flag="--platform=linux/arm64"
+		;;
+	pc)
+		base_img="$pc_base_img"		
+		platform_flag=""
+		;;
+	mac)
+		base_img="$macos_base_img"
+		platform_flag=""
+		;;
+	*)
+		echo "Usage: $0 [pi|pc|mac]" && exit 1 ;;
 esac
 
-docker build --build-arg BASE_IMAGE="$base_img" -t gbook-img .
+echo "Using base image: $base_img"
+echo "Using platform flag: $platform_flag"
+
+docker build $platform_flag --build-arg BASE_IMAGE="$base_img" -t gbook-img .
